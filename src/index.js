f4c632dd14054eb6e618e3a35f2356287836cf14
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-18 18:54:08 +0200

    Configure express-handlebars

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const PORT = 5000;
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: false});
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}....`));
