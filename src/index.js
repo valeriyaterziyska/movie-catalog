4e0a386d588749f4bf75d45440c0114fcf6413e7
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-19 10:15:54 +0200

    Fix paths

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
-const path = require("path");
 
-const PORT = 5000;
-
-const router = require('./routes');
+const configHandlebars = require("./config/configHandlebars");
+const configExpress = require("./config/configExpress");
+const routes = require("./routes");
 
 const app = express();
+const PORT = 5000;
 
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
+configHandlebars(app);
+configExpress(app);
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(router);
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}....`));
