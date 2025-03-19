12bb8c5693e602e3b4977eff2daad1391f7e94fb
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-19 12:01:10 +0200

    Add search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -16,11 +16,29 @@ exports.getAll = () => {
   return [...movies];
 };
 
+exports.search = (title, genre, year) => {
+  let result = [...movies];
+
+  if(title) {
+    result = result.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+  }
+
+  if(genre) {
+    result = result.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+  }
+
+  if(year) {
+    result = result.filter(movie => movie.year === year);
+  }
+
+  return result;
+};
+
 exports.getOne = (movieId) => {
-  const movie = movies.find(movie => movie._id == movieId);
+  const movie = movies.find((movie) => movie._id == movieId);
 
   return movie;
-}
+};
 
 exports.create = (movieData) => {
   movieData._id = movies[movies.length - 1]._id + 1;
