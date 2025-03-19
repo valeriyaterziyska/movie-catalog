f8dacd53561080b59b7a4019369c3dfe608bc0f2
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-19 10:40:14 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,17 @@
-const movies = [];
+const movies = [{
+    title: "Jungle cruise",
+    genre: "Adventure",
+    director: "Spilberg",
+    year: "2015",
+    imageUrl: "/img/jungle-cruise.jpeg",
+    rating: "4",
+    description:
+      "Description: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.",
+  },
+];
 
 exports.create = (movieData) => {
-    movies.push(movieData);
-}
+  console.log(movieData);
+
+  movies.push(movieData);
+};
