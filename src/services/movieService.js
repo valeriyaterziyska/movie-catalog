4f88482164c2cc0de06fe064292f61e2bbf498ab
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-24 11:21:20 +0200

    Refactor movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,25 +1,10 @@
 const Movie = require("../models/Movie");
 
-const movies = [
-  {
-    _id: 1,
-    title: "Jungle cruise",
-    genre: "Adventure",
-    director: "Spilberg",
-    year: "2015",
-    imageUrl: "/img/jungle-cruise.jpeg",
-    rating: 4,
-    description:
-      "Description: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.",
-  },
-];
+exports.getAll = () => Movie.find();
 
-exports.getAll = () => {
-  return [...movies];
-};
-
-exports.search = (title, genre, year) => {
-  let result = [...movies];
+//TODO: Filter result in mongoDB
+exports.search = async (title, genre, year) => {
+  let result = await Movie.find().lean();
 
   if (title) {
     result = result.filter((movie) =>
@@ -40,14 +25,6 @@ exports.search = (title, genre, year) => {
   return result;
 };
 
-exports.getOne = (movieId) => {
-  const movie = movies.find((movie) => movie._id == movieId);
+exports.getOne = (movieId) => Movie.findById(movieId);
 
-  return movie;
-};
-
-exports.create = async (movieData) => {
-  const result = await Movie.create(movieData);
-
-  return result;
-};
+exports.create = (movieData) => Movie.create(movieData);
