5cd88a07fa0c24db8043d5967e30ce15b89209ac
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-25 12:38:04 +0200

    Move details view

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -31,7 +31,7 @@ router.get("/movies/:movieId", async (req, res) => {
     movie.rating = new Array(Number(movie.rating)).fill(true);
     // movie.ratingStars = '&#x2605;'.repeat(movie.rating);
 
-    res.render("details", { movie });
+    res.render("movie/details", { movie });
 });
 
 router.get("/movies/:movieId/attach", isAuth, async (req, res) => {
