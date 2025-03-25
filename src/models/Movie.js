dad9ed9cd0e6b13cfedb6a76522305dd6f19f560
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-25 12:43:54 +0200

    Add relation from movie to user

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,45 +1,51 @@
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-    lowercase: true,
-  },
-  director: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-    min: 1900,
-    max: 2030,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  description: {
-    type: String,
-    required: true,
-    maxLength: 1000,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-    match: /^https?:\/\//,
-  },
-  casts: [{
-    type: mongoose.Types.ObjectId,
-    ref: 'Cast'
-  }]
+    title: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+        min: 1900,
+        max: 2030,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 1000,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        match: /^https?:\/\//,
+    },
+    casts: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Cast",
+        },
+    ],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    },
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
