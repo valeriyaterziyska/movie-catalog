d489322908e98a037db5ec571124afbd32c80f01
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-25 10:44:16 +0200

    Generate jwt token on successful login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,19 +3,27 @@ const router = require("express").Router();
 const authService = require("../services/authService");
 
 router.get("/register", (req, res) => {
-  res.render("auth/register");
+    res.render("auth/register");
 });
 
 router.post("/register", async (req, res) => {
-  const userData = req.body;
+    const userData = req.body;
 
-  await authService.register(userData);
+    await authService.register(userData);
 
-  res.redirect("/auth/login");
+    res.redirect("/auth/login");
 });
 
 router.get("/login", (req, res) => {
-    res.render('auth/login');
+    res.render("auth/login");
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const token = await authService.login(email, password);
+    console.log(token);
+
+    res.redirect('/');
 });
 
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,31 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt");
 
+const SECRET = "kajhsdktaweugabsjkzjgcfisad234wesdf";
+
+//TODO: Check if user exists
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+    // Get user from db
+    const user = await User.findOne({ email });
+
+    // Check if user exists
+    if (!user) {
+        throw new Error("Cannot find email or password");
+    }
+    // Check if password is valid
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error("Cannot find email or password");
+    }
+    // Generate jwt token
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    };
+    const token = await jwt.sign(payload, SECRET, { expiresIn: "2h" });
+    // return token
+    return token;
+};
